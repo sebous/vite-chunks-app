811abbdaf7a23f1d88054e29ebcbbe8651125013
sebous <sebous@gmail.com>
2022-01-31 17:24:08 +0100

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState, lazy, Suspense } from "react";
-import logo from "./logo.svg";
+import { lazy, Suspense } from "react";
 import "./App.css";
 // const SelectLazy = lazy(() => import("./SelectComponent"));
 
@@ -7,33 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
         {/* <Suspense fallback="loading">
           <SelectLazy />
         </Suspense> */}
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
     </div>
   );
